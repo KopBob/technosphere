c494a2f1bd0d8bbaace0bfb0272070310b642cc6
Boris Kopin <kopbob@gmail.com>
2016-03-21 16:53:11 +0300

    IR HW2 fix

--- information-retrieval/hw3/dupli.py
+++ information-retrieval/hw3/dupli.py
@@ -6,7 +6,6 @@ from collections import OrderedDict, defaultdict
 
 import mmh3
 
-from src.broder_shingles import MinshinglesCounter
 from src.docreader import DocumentStreamReader
 
 import psutil
@@ -44,21 +43,14 @@ def shingles2sketch(shingles, m_baskets=20):
     return sorted(baskets.values())
 
 
-@profile
 def doc2counter(docs_sketches, docs_ids):
     grouped_docs = defaultdict(list)
 
-    # print("Info: ", "stage1..", curr_mem(), file=sys.stderr)
     for doc_id in docs_sketches.keys():
         for shingle in docs_sketches[doc_id]:
             grouped_docs[shingle].append(doc_id)
 
         del docs_sketches[doc_id]
-
-    # for doc_id, doc_sketch in docs_sketches.iteritems():
-    #     for shingle in doc_sketch:
-    #         grouped_docs[shingle].append(doc_id)
-
     grouped_docs_cleaned = filter(lambda l: len(l) > 1, grouped_docs.itervalues())
 
     grouped_docs_cleaned = map(lambda x: sorted(x), grouped_docs_cleaned)
@@ -66,59 +58,33 @@ def doc2counter(docs_sketches, docs_ids):
 
     paired_docs_reduced = itertools.chain.from_iterable(paired_docs)
 
-    # print("Info: ", "stage2..", curr_mem(), type(paired_docs_reduced), file=sys.stderr)
-
     counter = defaultdict(int)
     for k in paired_docs_reduced:
         counter[k] += 1
 
-    # print("Info: ", "stage3..", curr_mem(), file=sys.stderr)
     ratio = lambda c: 1.0 * c / (1.0 * c + 2 * (20.0 - c))
 
     counter_filtered = [(docs_ids[id1], docs_ids[id2], ratio(c)) for ((id1, id2), c) in counter.iteritems() if
                         c > 17]
     counter_cleaned = sorted(counter_filtered, key=lambda x: x[2], reverse=True)
 
-    # print("Info: ", "stage4..", curr_mem(), file=sys.stderr)
     return counter_cleaned
 
 
-@profile
 def all_in_one(dataset):
     docs_ids = OrderedDict()
     docs_sketches = defaultdict(dict)
 
-    minhash_counter = MinshinglesCounter()
-
-    # print("Info: ", "parsing files..", curr_mem(), file=sys.stderr)
     for part in dataset[:]:
         reader = DocumentStreamReader(part)
-        # with gzip.open(part, 'rb') as f:
-        #     buffer = cStringIO.StringIO(f.read())
-        #     reader = DocumentStreamReader(buffer)
-
         for i, doc in enumerate(reader):
-            sketch = minhash_counter.count(doc.text)
+            doc_id = mmh3.hash(doc.url)
 
-            if sketch:
-                doc_id = mmh3.hash(doc.url)
+            docs_ids[doc_id] = doc.url
 
-                docs_ids[doc_id] = doc.url
-                # docs_sketches[mmh3.hash(doc.url)] = sketch
+            docs_sketches[mmh3.hash(doc.url)] = shingles2sketch(tokens2shingles(text2tokens(doc.text.strip())))
 
-                docs_sketches[mmh3.hash(doc.url)] = shingles2sketch(tokens2shingles(text2tokens(doc.text.strip())))
-                # {
-                #   'url': doc.url,
-                #  'sketch': shingles2sketch(tokens2shingles(text2tokens(doc.text.strip()))),
-                #                 'sketch': shingles2sketch(tokens2shingles(text2tokens(parse_html(doc.body).strip()))),
-                #                 'tokens': text2tokens(doc.text.strip()),
-                #                 'tokens': text2tokens(parse_html(doc.body).strip()),
-                # }
-
-    # print("Info: ", "counting..", curr_mem(), file=sys.stderr)
     result = doc2counter(docs_sketches, docs_ids)
 
-    # print("Info: ", "finish..", curr_mem(), file=sys.stderr)
-    #
-    # for row in result:
-    #     print("%s %s %f" % row)
+    for row in result:
+        print("%s %s %f" % row)
--- information-retrieval/hw3/run.py
+++ information-retrieval/hw3/run.py
@@ -5,14 +5,14 @@ import sys
 from dupli import all_in_one
 
 if __name__ == '__main__':
-    # files = sys.argv[1:]
-    # all_in_one(files)
+    files = sys.argv[1:]
+    all_in_one(files)
 
-    from os import listdir
-    from os.path import isfile, join
-
-    PATH_TO_DATASET = "./dataset/"
-
-    tarballs = ["".join((PATH_TO_DATASET, f)) for f in listdir(PATH_TO_DATASET) if isfile(join(PATH_TO_DATASET, f))]
-
-    all_in_one(tarballs[:2])
+    # from os import listdir
+    # from os.path import isfile, join
+    #
+    # PATH_TO_DATASET = "./dataset/"
+    #
+    # tarballs = ["".join((PATH_TO_DATASET, f)) for f in listdir(PATH_TO_DATASET) if isfile(join(PATH_TO_DATASET, f))]
+    #
+    # all_in_one(tarballs[:2])
